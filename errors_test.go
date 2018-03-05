d104146e8ffce4ab16e6b9e8db003df5656ef602
peter.verschuure <peter.verschuure@profects.com>
2018-03-05 11:27:20 +0100

    Fix record not found error check and add test

--- errors_test.go
+++ errors_test.go
@@ -18,3 +18,15 @@ func TestErrorsCanBeUsedOutsideGorm(t *testing.T) {
 		t.Fatalf("Gave wrong error, got %s", gErrs.Error())
 	}
 }
+
+func TestIsRecordNotFoundError(t *testing.T) {
+	err := errors.New("record not found")
+	if !gorm.IsRecordNotFoundError(err) {
+		t.Fatal("The check for a single record not found error returned false while expecting true")
+	}
+
+	errs := gorm.Errors([]error{errors.New("record not found")})
+	if !gorm.IsRecordNotFoundError(errs) {
+		t.Fatal("The check for a record not found error returned false while expecting true")
+	}
+}
